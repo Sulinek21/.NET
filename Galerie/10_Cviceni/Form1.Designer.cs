224e6bde97d756238e60761754b6980c5988d765
Tadeáš Fejt <76255913+Sulinek21@users.noreply.github.com>
2023-09-08 19:39:17 +0200

    Add files via upload

---
+++ Galerie/10_Cviceni/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿
+namespace _10_Cviceni
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbObrVloz3 = new System.Windows.Forms.RadioButton();
+            this.rbObrVloz2 = new System.Windows.Forms.RadioButton();
+            this.rbObrVloz1 = new System.Windows.Forms.RadioButton();
+            this.btnVloz = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.rbObrSmaz3 = new System.Windows.Forms.RadioButton();
+            this.rbObrSmaz2 = new System.Windows.Forms.RadioButton();
+            this.rbObrSmaz1 = new System.Windows.Forms.RadioButton();
+            this.btnSmaz = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbObrVloz3);
+            this.groupBox1.Controls.Add(this.rbObrVloz2);
+            this.groupBox1.Controls.Add(this.rbObrVloz1);
+            this.groupBox1.Controls.Add(this.btnVloz);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(221, 176);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Vložení obrázku";
+            // 
+            // rbObrVloz3
+            // 
+            this.rbObrVloz3.AutoSize = true;
+            this.rbObrVloz3.Location = new System.Drawing.Point(6, 98);
+            this.rbObrVloz3.Name = "rbObrVloz3";
+            this.rbObrVloz3.Size = new System.Drawing.Size(80, 19);
+            this.rbObrVloz3.TabIndex = 3;
+            this.rbObrVloz3.TabStop = true;
+            this.rbObrVloz3.Text = "3. Obrázek";
+            this.rbObrVloz3.UseVisualStyleBackColor = true;
+            // 
+            // rbObrVloz2
+            // 
+            this.rbObrVloz2.AutoSize = true;
+            this.rbObrVloz2.Location = new System.Drawing.Point(6, 73);
+            this.rbObrVloz2.Name = "rbObrVloz2";
+            this.rbObrVloz2.Size = new System.Drawing.Size(80, 19);
+            this.rbObrVloz2.TabIndex = 2;
+            this.rbObrVloz2.TabStop = true;
+            this.rbObrVloz2.Text = "2. Obrázek";
+            this.rbObrVloz2.UseVisualStyleBackColor = true;
+            // 
+            // rbObrVloz1
+            // 
+            this.rbObrVloz1.AutoSize = true;
+            this.rbObrVloz1.Location = new System.Drawing.Point(6, 48);
+            this.rbObrVloz1.Name = "rbObrVloz1";
+            this.rbObrVloz1.Size = new System.Drawing.Size(80, 19);
+            this.rbObrVloz1.TabIndex = 1;
+            this.rbObrVloz1.TabStop = true;
+            this.rbObrVloz1.Text = "1. Obrázek";
+            this.rbObrVloz1.UseVisualStyleBackColor = true;
+            // 
+            // btnVloz
+            // 
+            this.btnVloz.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnVloz.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnVloz.Location = new System.Drawing.Point(127, 50);
+            this.btnVloz.Name = "btnVloz";
+            this.btnVloz.Size = new System.Drawing.Size(88, 67);
+            this.btnVloz.TabIndex = 0;
+            this.btnVloz.Text = "Vložit obrázek";
+            this.btnVloz.UseVisualStyleBackColor = false;
+            this.btnVloz.Click += new System.EventHandler(this.btnVloz_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.rbObrSmaz3);
+            this.groupBox2.Controls.Add(this.rbObrSmaz2);
+            this.groupBox2.Controls.Add(this.rbObrSmaz1);
+            this.groupBox2.Controls.Add(this.btnSmaz);
+            this.groupBox2.Location = new System.Drawing.Point(265, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(221, 175);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Smazání obrázku";
+            // 
+            // rbObrSmaz3
+            // 
+            this.rbObrSmaz3.AutoSize = true;
+            this.rbObrSmaz3.Location = new System.Drawing.Point(6, 96);
+            this.rbObrSmaz3.Name = "rbObrSmaz3";
+            this.rbObrSmaz3.Size = new System.Drawing.Size(80, 19);
+            this.rbObrSmaz3.TabIndex = 3;
+            this.rbObrSmaz3.TabStop = true;
+            this.rbObrSmaz3.Text = "3. Obrázek";
+            this.rbObrSmaz3.UseVisualStyleBackColor = true;
+            // 
+            // rbObrSmaz2
+            // 
+            this.rbObrSmaz2.AutoSize = true;
+            this.rbObrSmaz2.Location = new System.Drawing.Point(6, 71);
+            this.rbObrSmaz2.Name = "rbObrSmaz2";
+            this.rbObrSmaz2.Size = new System.Drawing.Size(80, 19);
+            this.rbObrSmaz2.TabIndex = 2;
+            this.rbObrSmaz2.TabStop = true;
+            this.rbObrSmaz2.Text = "2. Obrázek";
+            this.rbObrSmaz2.UseVisualStyleBackColor = true;
+            // 
+            // rbObrSmaz1
+            // 
+            this.rbObrSmaz1.AutoSize = true;
+            this.rbObrSmaz1.Location = new System.Drawing.Point(6, 46);
+            this.rbObrSmaz1.Name = "rbObrSmaz1";
+            this.rbObrSmaz1.Size = new System.Drawing.Size(80, 19);
+            this.rbObrSmaz1.TabIndex = 1;
+            this.rbObrSmaz1.TabStop = true;
+            this.rbObrSmaz1.Text = "1. Obrázek";
+            this.rbObrSmaz1.UseVisualStyleBackColor = true;
+            this.rbObrSmaz1.CheckedChanged += new System.EventHandler(this.rbObrSmaz1_CheckedChanged);
+            // 
+            // btnSmaz
+            // 
+            this.btnSmaz.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnSmaz.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnSmaz.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnSmaz.Location = new System.Drawing.Point(127, 48);
+            this.btnSmaz.Name = "btnSmaz";
+            this.btnSmaz.Size = new System.Drawing.Size(88, 67);
+            this.btnSmaz.TabIndex = 0;
+            this.btnSmaz.Text = "Smazat obrázek";
+            this.btnSmaz.UseVisualStyleBackColor = false;
+            this.btnSmaz.Click += new System.EventHandler(this.btnSmaz_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 193);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(154, 129);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.DoubleClick += new System.EventHandler(this.pictureBox1_DoubleClick);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(172, 193);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(154, 129);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            this.pictureBox2.DoubleClick += new System.EventHandler(this.pictureBox2_DoubleClick);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox3.Location = new System.Drawing.Point(332, 193);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(154, 129);
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            this.pictureBox3.DoubleClick += new System.EventHandler(this.pictureBox3_DoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(494, 328);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Galerie";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbObrVloz3;
+        private System.Windows.Forms.RadioButton rbObrVloz2;
+        private System.Windows.Forms.RadioButton rbObrVloz1;
+        private System.Windows.Forms.Button btnVloz;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton rbObrSmaz3;
+        private System.Windows.Forms.RadioButton rbObrSmaz2;
+        private System.Windows.Forms.RadioButton rbObrSmaz1;
+        private System.Windows.Forms.Button btnSmaz;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ Galerie/10_Cviceni/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _10_Cviceni
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        //VLOŽENÍ OBRÁZKU
+        private void btnVloz_Click(object sender, EventArgs e)
+        {
+            if (this.rbObrVloz1.Checked && this.pictureBox1.Image != null)
+            {
+                int num1 = (int)MessageBox.Show("PictureBox1 není volný!");
+            }
+            else if (this.rbObrVloz1.Checked && this.pictureBox1.Image == null)
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                openFileDialog.Filter = "Obrázky (*.jpg; *.png)|*.jpg; *.png";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    this.pictureBox1.Image = Image.FromFile(openFileDialog.FileName);
+                    this.pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                }
+            }
+            if (this.rbObrVloz2.Checked && this.pictureBox2.Image != null)
+            {
+                int num2 = (int)MessageBox.Show("PictureBox2 není volný!");
+            }
+            else if (this.rbObrVloz2.Checked && this.pictureBox2.Image == null)
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                openFileDialog.Filter = "Obrázky (*.jpg; *.png)|*.jpg; *.png";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    this.pictureBox2.Image = Image.FromFile(openFileDialog.FileName);
+                    this.pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+                }
+            }
+            if (this.rbObrVloz3.Checked && this.pictureBox3.Image != null)
+            {
+                int num3 = (int)MessageBox.Show("PictureBox3 není volný!");
+            }
+            else
+            {
+                if (!this.rbObrVloz3.Checked || this.pictureBox3.Image != null)
+                    return;
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                openFileDialog.Filter = "Obrázky (*.jpg; *.png)|*.jpg; *.png";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    this.pictureBox3.Image = Image.FromFile(openFileDialog.FileName);
+                    this.pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+                }
+            }
+        }
+
+        //OTEVŘENÍ NOVÉHO FORM2 - DOUBLECLICK
+        private static void Okno(PictureBox pic)
+        {
+            Form2 form2 = new Form2();
+            form2.BackgroundImage = pic.Image;
+            form2.BackgroundImageLayout = ImageLayout.Stretch;
+            int num = (int)form2.ShowDialog();
+        }
+
+        private void pictureBox1_DoubleClick(object sender, EventArgs e)
+        {
+            Okno(this.pictureBox1);
+        }
+
+        private void pictureBox2_DoubleClick(object sender, EventArgs e)
+        {
+            Okno(this.pictureBox2);
+        }
+
+        private void pictureBox3_DoubleClick(object sender, EventArgs e)
+        {
+            Okno(this.pictureBox3);
+        }
+
+
+        //MAZÁNÍ OBRÁZKŮ
+        private void btnSmaz_Click(object sender, EventArgs e)
+        {
+            if (this.rbObrSmaz1.Checked)
+                this.pictureBox1.Image = (Image)null;
+            if (this.rbObrSmaz2.Checked)
+                this.pictureBox2.Image = (Image)null;
+            if (!this.rbObrSmaz3.Checked)
+                return;
+            this.pictureBox3.Image = (Image)null;
+        }
+
+
+        //ZAŠKRTNUTÍ RADIOBUTTONU
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            this.rbObrVloz1.Checked = true;
+            this.rbObrSmaz1.Checked = true;
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            this.rbObrVloz2.Checked = true;
+            this.rbObrSmaz2.Checked = true;
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            this.rbObrVloz3.Checked = true;
+            this.rbObrSmaz3.Checked = true;
+        }
+
+        private void rbObrSmaz1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
